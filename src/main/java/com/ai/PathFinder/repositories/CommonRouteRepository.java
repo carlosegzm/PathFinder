95335f7a871117864c4b77f679432beb58d07173
Cadu <boli69112@gmail.com>
2026-02-27 19:27:38 -0300

    feat: add repositories for routes and paths.

---
+++ src/main/java/com/ai/PathFinder/repositories/CommonRouteRepository.java
@@ -0,0 +1,12 @@
+package com.ai.PathFinder.repositories;
+
+import com.ai.PathFinder.entities.CommonRoute;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommonRouteRepository extends JpaRepository<CommonRoute, Long> {
+
+
+
+}
---
+++ src/main/java/com/ai/PathFinder/repositories/PathBetweenCapitalsRepository.java
@@ -0,0 +1,9 @@
+package com.ai.PathFinder.repositories;
+
+import com.ai.PathFinder.entities.PathBetweenCapitals;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PathBetweenCapitalsRepository extends JpaRepository<PathBetweenCapitals, Long> {
+
+
+}

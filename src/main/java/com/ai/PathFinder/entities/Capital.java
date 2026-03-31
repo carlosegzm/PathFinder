4001dcdc2ab19f1b50d0f59205f9176fc1bc7e68
Giordan Ebertz <ebertzgiordan@gmail.com>
2026-03-30 21:32:08 -0300

    chore: update pom.xml: fix spring boot version and test dependencies

--- src/main/java/com/ai/PathFinder/entities/Capital.java
+++ src/main/java/com/ai/PathFinder/entities/Capital.java
@@ -1,22 +1,30 @@
 package com.ai.PathFinder.entities;
 
-import java.math.BigDecimal;
-
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+import java.math.BigDecimal;
+
+@Getter
+@Setter
 @Entity
 @Table(name = "capitals")
 public class Capital {
 
     @Id
+    @Column(name = "id", length = 2)
     private String id;
-    
-    private String name;
-    private BigDecimal latitude;
-    private BigDecimal longitude;
 
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+
+    @Column(name = "latitude", nullable = false, precision = 10, scale = 8)
+    private BigDecimal latitude;
+
+    @Column(name = "longitude", nullable = false, precision = 11, scale = 8)
+    private BigDecimal longitude;
 }

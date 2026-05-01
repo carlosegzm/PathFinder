dca58bc9d64d57b632e4a36929e4c8e43f734169
Cadu <boli69112@gmail.com>
2026-05-01 18:37:06 -0300

    docs: add comments to Kruskal and UnionFind classes

--- src/main/java/com/ai/PathFinder/strategy/kruskal/Kruskal.java
+++ src/main/java/com/ai/PathFinder/strategy/kruskal/Kruskal.java
@@ -22,10 +22,11 @@ public class Kruskal {
     private final PathBetweenCapitalsRepository pathRepository;
     private final CapitalRepository capitalRepository;
 
-    // Otimização na inicialização
+    //guarda os dados na memória
     private List<PathBetweenCapitals> allPaths;
     private List<Capital> allCapitals;
 
+    //busca no banco de dados qnd a aplicação liga
     @EventListener(ApplicationReadyEvent.class)
     public void init(){
         this.allPaths = pathRepository.findAll();
@@ -35,6 +36,8 @@ public class Kruskal {
     public KruskalResponseDto executeKruskal() {
 
         UnionFind uf = new UnionFind();
+
+        //coloca cada capital isolada no começo
         for (Capital capital : allCapitals) {
             uf.makeSet(capital.getId());
         }
@@ -46,33 +49,41 @@ public class Kruskal {
 
         Set<String> railwayNetwork = new HashSet<>();
 
+        //testa cada estrada da lista
         for (PathBetweenCapitals path : allPaths) {
             String originId = path.getOrigin().getId();
             String destinationId = path.getDestination().getId();
 
+            //verifica se o destino e a origem já estão conectadas (p não criar ciclos)
             if (!uf.find(originId).equals(uf.find(destinationId))) {
 
+                //une as duas capitais
                 uf.union(originId, destinationId);
 
                 String id1 = path.getOrigin().getId();
                 String id2 = path.getDestination().getId();
 
+                // padroniza a string da rota em ordem alfabética (ex: RS-SC)
                 railwayNetwork.add(
                     (id1.compareTo(id2) < 0) ? id1 + "-" + id2 : id2 + "-" + id1
                 );
 
+                //soma a distância desse trecho na distância total
                 totalDistance += path.getDistance();
             } 
         }
 
+        // calcula o custo total da obra
         BigDecimal distance = BigDecimal.valueOf(totalDistance);
         BigDecimal costPerKm = new BigDecimal("2000000.00"); //2.000.000 por km
 
         BigDecimal totalConstructionCost = distance.multiply(costPerKm);
 
+        // define que o orçamento para o Genético será 60%
         BigDecimal percentage = new BigDecimal("0.60"); //60% custo total
         BigDecimal availableBudgetForGenetics = totalConstructionCost.multiply(percentage);
 
+        //prepara o response
         KruskalResponseDto response = new KruskalResponseDto(totalDistance, totalConstructionCost, availableBudgetForGenetics, railwayNetwork);
 
         return response;
--- src/main/java/com/ai/PathFinder/strategy/kruskal/UnionFind.java
+++ src/main/java/com/ai/PathFinder/strategy/kruskal/UnionFind.java
@@ -3,29 +3,37 @@ package com.ai.PathFinder.strategy.kruskal;
 import java.util.HashMap;
 import java.util.Map;
 
+// ajuda a agrupar as capitais e verificar se duas cidades já estão conectadas (evitando a criação de ciclos)
 public class UnionFind {
 
+    // mapeia o nó para o seu nó pai
     private Map<String, String> parent = new HashMap<>();
 
+    // cria um novo conjunto onde o elemento é o seu próprio pai
     public void makeSet(String id) {
         parent.put(id, id);
     }
 
+    // retorna o pai do conjunto ao qual o id pertence.
     public String find(String id) {
 
+        // se a capital aponta para ela mesma, ela é o pai
         if (parent.get(id).equals(id)) {
             return id;
         }
 
+        // se não for, procura o pai e já atualiza para ficar mais rápido na próxima
         String root = find(parent.get(id));
         parent.put(id, root);
         return root;
     }
 
+    // junta duas capitais no mesmo grupo
     public void union(String id1, String id2) {
         String root1 = find(id1);
         String root2 = find(id2);
 
+        // só faz a união se elas ainda estiverem em grupos separados
         if (!root1.equals(root2)) {
             parent.put(root1, root2);
         }
